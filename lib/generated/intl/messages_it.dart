2b6154e11031f7e2cb9c12cf941e4d7f56e20753
Emilio Dalla Torre <info@emiliodallatorre.it>
2021-05-06 18:04:53 +0200

    Aggiornati tutti i pacchetti.

--- lib/generated/intl/messages_it.dart
+++ lib/generated/intl/messages_it.dart
@@ -7,7 +7,7 @@
 // ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
@@ -20,18 +20,21 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'it';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-
-  static _notInlinedMessages(_) => <String, Function>{
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "add": MessageLookupByLibrary.simpleMessage("Aggiungi"),
         "addImage": MessageLookupByLibrary.simpleMessage("Aggiungi immagine"),
-        "authors": MessageLookupByLibrary.simpleMessage("Autori, separati da una virgola"),
-        "bookError": MessageLookupByLibrary.simpleMessage("C\'è stato un errore con questo libro"),
-        "bookNotFoundError": MessageLookupByLibrary.simpleMessage("Libro non trovato"),
+        "authors": MessageLookupByLibrary.simpleMessage(
+            "Autori, separati da una virgola"),
+        "bookError": MessageLookupByLibrary.simpleMessage(
+            "C\'è stato un errore con questo libro"),
+        "bookNotFoundError":
+            MessageLookupByLibrary.simpleMessage("Libro non trovato"),
         "bookState": MessageLookupByLibrary.simpleMessage("Stato del libro"),
         "buyBooks": MessageLookupByLibrary.simpleMessage("Le mie ricerche"),
         "cancelScan": MessageLookupByLibrary.simpleMessage("Stop"),
         "chats": MessageLookupByLibrary.simpleMessage("Conversazioni"),
-        "chooseSource": MessageLookupByLibrary.simpleMessage("Scegli sorgente immagine"),
+        "chooseSource":
+            MessageLookupByLibrary.simpleMessage("Scegli sorgente immagine"),
         "chooseSourceExtended": MessageLookupByLibrary.simpleMessage(
             "Per favore, acquisisci o seleziona una sorgente per l\'immagine di copertina del libro. Quest\'immagine sarà pubblicamente visibile."),
         "city": MessageLookupByLibrary.simpleMessage("Città"),
@@ -42,55 +45,76 @@ class MessageLookup extends MessageLookupByLibrary {
         "enable": MessageLookupByLibrary.simpleMessage("Abilita"),
         "enableProximityBooks": MessageLookupByLibrary.simpleMessage(
             "Vuoi abilitare la ricerca di prossimità?\nTi sarà utile per trovare i libri che cerchi vicino a te.\nSe non attiverai questa funzionalità ora, potrai sempre farlo nelle impostazioni dell\'app."),
-        "getInTouchWithSeller": MessageLookupByLibrary.simpleMessage("Contatta il venditore"),
-        "highlight": MessageLookupByLibrary.simpleMessage("Il libro presenta segni di evidenziatore"),
+        "getInTouchWithSeller":
+            MessageLookupByLibrary.simpleMessage("Contatta il venditore"),
+        "highlight": MessageLookupByLibrary.simpleMessage(
+            "Il libro presenta segni di evidenziatore"),
         "insertIsbn": MessageLookupByLibrary.simpleMessage("Inserisci ISBN"),
         "isbn": MessageLookupByLibrary.simpleMessage("ISBN"),
-        "isbnLengthError": MessageLookupByLibrary.simpleMessage("Gli ISBN sono di 10 o 13 caratteri"),
+        "isbnLengthError": MessageLookupByLibrary.simpleMessage(
+            "Gli ISBN sono di 10 o 13 caratteri"),
         "km": MessageLookupByLibrary.simpleMessage("km"),
         "login": MessageLookupByLibrary.simpleMessage("Accedi"),
         "logout": MessageLookupByLibrary.simpleMessage("Logout"),
         "manualAdd": MessageLookupByLibrary.simpleMessage("Aggiunta manuale"),
         "name": MessageLookupByLibrary.simpleMessage("Nome"),
-        "nearbyBooks": MessageLookupByLibrary.simpleMessage("Libri nei dintorni"),
-        "noBooks": MessageLookupByLibrary.simpleMessage("Non c\'è ancora nessun libro in questa sezione, aggiungilo con l\'apposito pulsante...!"),
-        "noChats": MessageLookupByLibrary.simpleMessage("Non hai ancora alcuna conversazione.\nCompariranno qui quelle che avvierai con gli altri utenti."),
-        "noIsbnScanned": MessageLookupByLibrary.simpleMessage("Nessun ISBN trovato"),
-        "noMessages": MessageLookupByLibrary.simpleMessage("Nessun messaggio in questa chat, scrivine uno per iniziare!"),
+        "nearbyBooks":
+            MessageLookupByLibrary.simpleMessage("Libri nei dintorni"),
+        "noBooks": MessageLookupByLibrary.simpleMessage(
+            "Non c\'è ancora nessun libro in questa sezione, aggiungilo con l\'apposito pulsante...!"),
+        "noChats": MessageLookupByLibrary.simpleMessage(
+            "Non hai ancora alcuna conversazione.\nCompariranno qui quelle che avvierai con gli altri utenti."),
+        "noIsbnScanned":
+            MessageLookupByLibrary.simpleMessage("Nessun ISBN trovato"),
+        "noMessages": MessageLookupByLibrary.simpleMessage(
+            "Nessun messaggio in questa chat, scrivine uno per iniziare!"),
         "noResults": MessageLookupByLibrary.simpleMessage(
             "Non abbiamo trovato nessun libro con questa query, riprova nei prossimi giorni: può darsi che qualche utente metta in vendita questo testo in futuro!"),
-        "noSearch": MessageLookupByLibrary.simpleMessage("Scrivi qualcosa per iniziare una nuova ricerca..."),
+        "noSearch": MessageLookupByLibrary.simpleMessage(
+            "Scrivi qualcosa per iniziare una nuova ricerca..."),
         "noSearchBooks": MessageLookupByLibrary.simpleMessage(
             "Al momento non hai ancora aggiunto alcun libro alla tua lista di ricerca, aggiungine uno col pulsante \"Cerca\" in alto a destra...!"),
         "note": MessageLookupByLibrary.simpleMessage("Note"),
         "ok": MessageLookupByLibrary.simpleMessage("Ok"),
-        "pen": MessageLookupByLibrary.simpleMessage("Sul libro è stata utilizzata la penna o altro marcatore indelebile"),
-        "pencil": MessageLookupByLibrary.simpleMessage("Il libro è stato annotato in matita"),
-        "phoneNumber": MessageLookupByLibrary.simpleMessage("Numero di telefono"),
+        "pen": MessageLookupByLibrary.simpleMessage(
+            "Sul libro è stata utilizzata la penna o altro marcatore indelebile"),
+        "pencil": MessageLookupByLibrary.simpleMessage(
+            "Il libro è stato annotato in matita"),
+        "phoneNumber":
+            MessageLookupByLibrary.simpleMessage("Numero di telefono"),
         "position": MessageLookupByLibrary.simpleMessage("Posizione"),
         "present": MessageLookupByLibrary.simpleMessage("Presente"),
         "proceed": MessageLookupByLibrary.simpleMessage("Procedi"),
-        "proximitySearch": MessageLookupByLibrary.simpleMessage("Ricerca di prossimità"),
+        "proximitySearch":
+            MessageLookupByLibrary.simpleMessage("Ricerca di prossimità"),
         "removeBookBecauseAlter": MessageLookupByLibrary.simpleMessage("Altro"),
-        "removeBookBecauseOtherSell": MessageLookupByLibrary.simpleMessage("Ho venduto questo libro esternamente a Bookaround"),
-        "removeBookBecauseSell": MessageLookupByLibrary.simpleMessage("Ho venduto questo libro tramite Bookaround"),
-        "removeBookFromSell": MessageLookupByLibrary.simpleMessage("Rimuovi dalla vendita"),
-        "removeBookSearch": MessageLookupByLibrary.simpleMessage("Rimuovere questo libro dalla ricerca?"),
-        "removeBookSearchExtended":
-            MessageLookupByLibrary.simpleMessage("Se rimuoverai questo libro dalla ricerca, non riceverai più notifiche quando un utente ne metterà in vendita una copia."),
-        "requiredField": MessageLookupByLibrary.simpleMessage("Questo campo è necessario"),
-        "responsibleUse": MessageLookupByLibrary.simpleMessage("Uso responsabile"),
+        "removeBookBecauseOtherSell": MessageLookupByLibrary.simpleMessage(
+            "Ho venduto questo libro esternamente a Bookaround"),
+        "removeBookBecauseSell": MessageLookupByLibrary.simpleMessage(
+            "Ho venduto questo libro tramite Bookaround"),
+        "removeBookFromSell":
+            MessageLookupByLibrary.simpleMessage("Rimuovi dalla vendita"),
+        "removeBookSearch": MessageLookupByLibrary.simpleMessage(
+            "Rimuovere questo libro dalla ricerca?"),
+        "removeBookSearchExtended": MessageLookupByLibrary.simpleMessage(
+            "Se rimuoverai questo libro dalla ricerca, non riceverai più notifiche quando un utente ne metterà in vendita una copia."),
+        "requiredField":
+            MessageLookupByLibrary.simpleMessage("Questo campo è necessario"),
+        "responsibleUse":
+            MessageLookupByLibrary.simpleMessage("Uso responsabile"),
         "responsibleUseExtended": MessageLookupByLibrary.simpleMessage(
             "Nel definire manualmente i dati di questo ISBN, accetti che gli utenti che dopo di te lo scansioneranno, possano attingere ai dati che ora inserirai.\nNon abbiamo la possibilità di controllare che ogni libro venga inserito correttamente: per favore, inserisci questi dati con cura."),
         "save": MessageLookupByLibrary.simpleMessage("Salva"),
         "searchBook": MessageLookupByLibrary.simpleMessage("Cerca un libro..."),
         "sellBooks": MessageLookupByLibrary.simpleMessage("I miei libri"),
-        "showcaseAddToWishlist":
-            MessageLookupByLibrary.simpleMessage("Usa questo pulsante per cercare i libri che stai cercando (per titolo, autore o ISBN) ed aggiungerli alla lista desideri"),
-        "showcaseChat": MessageLookupByLibrary.simpleMessage("In questa sezione potrai vedere le chat che hai iniziato con gli altri utenti"),
+        "showcaseAddToWishlist": MessageLookupByLibrary.simpleMessage(
+            "Usa questo pulsante per cercare i libri che stai cercando (per titolo, autore o ISBN) ed aggiungerli alla lista desideri"),
+        "showcaseChat": MessageLookupByLibrary.simpleMessage(
+            "In questa sezione potrai vedere le chat che hai iniziato con gli altri utenti"),
         "showcaseFloatingActionButton": MessageLookupByLibrary.simpleMessage(
             "Usa questo pulsante per aggiungere i libri che vuoi vendere. Scansiona il relativo ISBN (il codice a barre) e Bookaround si occuperà di tutto."),
-        "showcaseSearchBottom": MessageLookupByLibrary.simpleMessage("In questa sezione potrai aggiungere libri alla tua wishlist e vederne le copie in vendita nei dintorni"),
+        "showcaseSearchBottom": MessageLookupByLibrary.simpleMessage(
+            "In questa sezione potrai aggiungere libri alla tua wishlist e vederne le copie in vendita nei dintorni"),
         "soldBy": MessageLookupByLibrary.simpleMessage("Venduto da"),
         "sourceCamera": MessageLookupByLibrary.simpleMessage("Fotocamera"),
         "sourceGallery": MessageLookupByLibrary.simpleMessage("Galleria"),
@@ -98,9 +122,13 @@ class MessageLookup extends MessageLookupByLibrary {
         "title": MessageLookupByLibrary.simpleMessage("Titolo del libro"),
         "undo": MessageLookupByLibrary.simpleMessage("Annulla"),
         "usage": MessageLookupByLibrary.simpleMessage("Utilizzo"),
-        "whereIsBook": MessageLookupByLibrary.simpleMessage("Dove si trova il libro?"),
-        "writeAMessage": MessageLookupByLibrary.simpleMessage("Scrivi un messaggio..."),
-        "wrongCode": MessageLookupByLibrary.simpleMessage("Questo codice non è corretto, riprova"),
-        "wrongField": MessageLookupByLibrary.simpleMessage("Inserisci correttamente questo dato")
+        "whereIsBook":
+            MessageLookupByLibrary.simpleMessage("Dove si trova il libro?"),
+        "writeAMessage":
+            MessageLookupByLibrary.simpleMessage("Scrivi un messaggio..."),
+        "wrongCode": MessageLookupByLibrary.simpleMessage(
+            "Questo codice non è corretto, riprova"),
+        "wrongField": MessageLookupByLibrary.simpleMessage(
+            "Inserisci correttamente questo dato")
       };
 }
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1,7 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
 import 'intl/messages_all.dart';
 
 // **************************************************************************
@@ -11,7 +10,7 @@ import 'intl/messages_all.dart';
 
 // ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
 // ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
-// ignore_for_file: avoid_redundant_argument_values
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
 
 class S {
   S();
@@ -19,14 +18,17 @@ class S {
   static S? _current;
 
   static S get current {
-    assert(_current != null, 'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    assert(_current != null,
+        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
     return _current!;
   }
 
   static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
 
   static Future<S> load(Locale locale) {
-    final name = (locale.countryCode?.isEmpty ?? false) ? locale.languageCode : locale.toString();
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
     final localeName = Intl.canonicalizedLocale(name);
     return initializeMessages(localeName).then((_) {
       Intl.defaultLocale = localeName;
@@ -39,7 +41,8 @@ class S {
 
   static S of(BuildContext context) {
     final instance = S.maybeOf(context);
-    assert(instance != null, 'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    assert(instance != null,
+        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
     return instance!;
   }
 
@@ -780,10 +783,8 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
 
   @override
   bool isSupported(Locale locale) => _isSupported(locale);
-
   @override
   Future<S> load(Locale locale) => S.load(locale);
-
   @override
   bool shouldReload(AppLocalizationDelegate old) => false;
 
