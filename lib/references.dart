92225da533a2d9915ecb7ea6589cd2c62240c2b3
Emilio Dalla Torre <info@emiliodallatorre.it>
2021-05-22 16:54:31 +0200

    Cambiato il link per invitare un amico al dominio generale.

--- lib/references.dart
+++ lib/references.dart
@@ -25,6 +25,6 @@ class References {
   static const String privacyPolicyUrl = "https://bookaround.app/privacy.html";
   static const String termsAndConditionsUrl = "https://bookaround.app/terms.html";
 
-  static const String sharableAppLink = "https://bookaround.app/inapp/get";
+  static const String sharableAppLink = "https://bookaround.app/";
   static const String copyrightString = "\u{a9} 2021 Emilio Dalla Torre";
 }
