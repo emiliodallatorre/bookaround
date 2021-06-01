01ad7cc45082b0e703c1ebe75ccd1756340edb3f
Emilio Dalla Torre <info@emiliodallatorre.it>
2021-06-01 21:24:24 +0200

    Implementato Firebase Analytics.

--- functions/src/notifier.ts
+++ functions/src/notifier.ts
@@ -1,4 +1,4 @@
-import * as admin from "firebase-admin";
+// import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
 export const notifySearchers = functions.firestore.document('/books/{bookId}').onCreate(async (snapshot, context) => {
--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ import 'package:bookaround/models/settings_model.dart';
 import 'package:bookaround/models/user_model.dart';
 import 'package:bookaround/references.dart';
 import 'package:bookaround/resources/provider/location_provider.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -38,6 +40,7 @@ class Bookaround extends StatelessWidget {
     ]);
 
     final MaterialColor mainColor = Colors.teal;
+    final FirebaseAnalytics analytics = FirebaseAnalytics();
 
     return MultiProvider(
       providers: [
@@ -76,6 +79,9 @@ class Bookaround extends StatelessWidget {
             ChatScreen.route: (BuildContext context) => ChatScreen(),
             WebViewScreen.route: (BuildContext context) => WebViewScreen(),
           },
+          navigatorObservers: [
+            FirebaseAnalyticsObserver(analytics: analytics),
+          ],
         ),
       ),
     );
